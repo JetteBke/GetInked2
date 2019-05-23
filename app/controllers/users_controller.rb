580ce6a22c365f957b39300640b668ba6a7a0cc3
Jette Bakemeier <jette.bakemeier@gmail.com>
2019-05-23 17:16:39 +0100

    fixed bugs in links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     authorize @user
     @tattoos = Tattoo.where(user: @user)
-    @artist = @tattoos.first.user
+    @artist = @tattoos.first.user if @user.role == "artist"
+    @bookings = policy_scope(Booking).order(created_at: :desc)
   end
 end
