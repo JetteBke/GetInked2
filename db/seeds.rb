58c0cfbe5f7467709e410df0835d6a9b9bd9aec6
Isabella Ebihara <isabella.ebihara@gmail.com>
2019-05-24 14:10:56 +0100

    the seeds you guys

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'started seeding'
-User.destroy_all
 Tattoo.destroy_all
+User.destroy_all
 puts 'destroyed all'
 
 url1 = 'https://www.tattooinsider.com/wp-content/uploads/2018/03/Butterfly-Tattoo-by-Alan-Victor.jpg'
@@ -15,6 +15,15 @@ url2 = 'http://www.spews.org/wp-content/uploads/2018/05/georgiagreynyc-4-800x600
 url3 = 'https://www.tattooinsider.com/wp-content/uploads/2017/12/Rose-Small-Tattoo-by-Romeo-Lacoste-.jpg'
 url4 = 'https://slack-imgs.com/?c=1&url=https%3A%2F%2Fres.cloudinary.com%2Fdj1bs7vyp%2Fimage%2Fupload%2Fv1558531816%2Fmtntattoos_dvhghe.jpg'
 url5 = 'https://slack-imgs.com/?c=1&url=https%3A%2F%2Fres.cloudinary.com%2Fdj1bs7vyp%2Fimage%2Fupload%2Fv1558531816%2FJess-Crop_n29xjx.jpg'
+url6 = 'https://res.cloudinary.com/dj1bs7vyp/image/upload/v1558700151/2.png'
+url7 = 'https://res.cloudinary.com/dj1bs7vyp/image/upload/v1558700151/1.png'
+url8 = 'https://res.cloudinary.com/dj1bs7vyp/image/upload/v1558700151/3.png'
+url9 = 'https://res.cloudinary.com/dj1bs7vyp/image/upload/v1558701295/Screen_Shot_2019-05-24_at_1.31.55_PM_nyt3fw.png'
+url10 = 'https://res.cloudinary.com/dj1bs7vyp/image/upload/v1558701294/Screen_Shot_2019-05-24_at_1.29.59_PM_tifofh.png'
+url11 = 'https://res.cloudinary.com/dj1bs7vyp/image/upload/v1558701293/images-2_uvqbik.jpg'
+url12 = 'https://res.cloudinary.com/dj1bs7vyp/image/upload/v1558701293/download_g0lkwv.jpg'
+url13 = 'https://res.cloudinary.com/dj1bs7vyp/image/upload/v1558701293/images-3_k7xzuy.jpg'
+url14 = 'https://res.cloudinary.com/dj1bs7vyp/image/upload/v1558701293/images-4_owbsuo.jpg'
 puts 'url done'
 
 users = User.create([{ email: "jackattack@gmail.com", role: "artist", name: "Jack Erins",  password: "123456", password_confirmation: "123456"},
@@ -25,8 +34,19 @@ users = User.create([{ email: "jackattack@gmail.com", role: "artist", name: "Jac
 puts 'user done'
 
 tattoos = Tattoo.create([{ remote_photo_url: url1, description: "a butterly", user: users.first},
-  { remote_photo_url: url2, description: "five triangles reduced in size with splatter paint", user: users.first},
+  { remote_photo_url: url2, description: "five triangles", user: users.first},
   { remote_photo_url: url3, description: "a mircroneedled rose tattoo", user: users.second},
   { remote_photo_url: url4, description: "landscape tattoo", user: users.second},
-  { remote_photo_url: url5, description: "flower on back", user: users.second}])
+  { remote_photo_url: url5, description: "flower on back", user: users.second},
+  { remote_photo_url: url6, description: "snake", user: users.first},
+  { remote_photo_url: url7, description: "hands", user: users.first},
+  { remote_photo_url: url8, description: "lion", user: users.second},
+  { remote_photo_url: url9, description: "heart lifeline", user: users.second},
+  { remote_photo_url: url10, description: "emo", user: users.first},
+  { remote_photo_url: url11, description: "fight club", user: users.first},
+  { remote_photo_url: url12, description: "stitch", user: users.second},
+  { remote_photo_url: url13, description: "shut up", user: users.second},
+  { remote_photo_url: url14, description: "we are all mad here", user: users.second}
+])
+
 puts 'finished'
